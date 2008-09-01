d3daa12f3517292757b8234c8fa601fac5b8dd3f
Matt Constantine <designmc@gmail.com>
2008-08-31 23:37:15 -0400

    eval_cfm does the right thing by default now

--- eval_cfm.rb
+++ eval_cfm.rb
@@ -1,9 +1,12 @@
 require 'lib/archaeopteryx'
 require 'cfm/cfm'
-
+require 'cfm/cfm_generator'
+stylefile = ARGV.shift || "cfm/styles/sample.rb"
 Arkx.new(:clock => Clock.new(120),
          :measures => 2,
          :logging => true,
          :evil_timer_offset_wtf => 0.2,
-         :generator => CFMGenerator.new(:stylefile => "cfm/styles/cfm_style.rb",
+         :generator => CFMGenerator.new(
+                                        :stylefile => stylefile,
+                                        #:stylefile => "cfm/styles/cfm_style.rb",
                                         :mutation => L{|measure| 0 == (measure - 1) % 16}) ).go
